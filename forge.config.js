c6cf2f755f2b43fb2eddb4a975c24718116a9309
ylber-gashi <ylberg37@gmail.com>
2024-09-14 20:10:37 +0200

    github action

--- forge.config.js
+++ forge.config.js
@@ -24,6 +24,18 @@ module.exports = {
       config: {},
     },
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'ylber-gashi',
+          name: 'electron-example',
+        },
+        prerelease: true,
+      }
+    }
+  ],
   plugins: [
     {
       name: '@electron-forge/plugin-auto-unpack-natives',
